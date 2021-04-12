308a72c00491fc15be976f43b2296ab1ce3798fb
Aram Mikayelian <arammikayelian@gmail.com>
2021-04-12 14:58:35 -0700

    Added CRUD for Campgrounds

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path')
 const mongoose = require('mongoose');
+const methodOverride = require("method-override")
 const Campground = require('./models/campground')
 
 mongoose.connect('mongodb://localhost:27017/campmate', {
@@ -25,6 +26,9 @@ app.set('view engine', 'ejs')
 //joining our views path to our working directory, establishing an absolute path to views
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded({extended: true}))
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) =>
 {
     res.render("home")
@@ -38,7 +42,48 @@ app.get('/campgrounds', async (req, res) =>
     res.render("campgrounds/index", {campgrounds})
 })
 
-//node listening and waiting in the background for requests
+app.get('/campgrounds/new', (req, res) =>
+{
+    res.render('campgrounds/new')
+})
+
+app.post('/campgrounds', async (req, res) =>
+{
+    const campground = new Campground(req.body.campground)
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.get('/campgrounds/:id', async (req, res) =>
+{
+    const id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/show', {campground})
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) =>
+{
+    const id = req.params.id;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', {campground})
+})
+
+//put route through methodOverride on form, with POST method
+app.put('/campgrounds/:id', async (req, res) =>
+{
+    const { id } = req.params;
+    //find and update by id and spread request body for changes
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground })
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+app.delete('/campgrounds/:id', async (req, res)=> {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id, {useFindAndModify: false})
+    res.redirect('/campgrounds')
+})
+
 app.listen(3000, () =>
 {
     console.log("Serving on Port 3000!")
