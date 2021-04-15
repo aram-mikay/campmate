7736ab8e4d2c462d1ef72c268673455365067b5d
Aram Mikayelian <arammikayelian@gmail.com>
2021-04-15 15:38:07 -0700

    Added utility functions for error handling, create async function wrapper and defined basic express error handler, added basic error template page with status code and message

--- app.js
+++ app.js
@@ -1,127 +1,113 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path')
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate')
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
 
-const methodOverride = require("method-override")
-const Campground = require('./models/campground')
-
-
-
-
-mongoose.connect('mongodb://localhost:27017/campmate', {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
-
-
+mongoose.connect("mongodb://localhost:27017/campmate", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
 
 const db = mongoose.connection;
 //checking for errors on mongoose connection to mongo
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () =>
-{   
-    console.log("Database Connected")
-})
+db.once("open", () => {
+  console.log("Database Connected");
+});
 
 //using ejs engine instead of default express engine
-app.engine('ejs', ejsMate);
+app.engine("ejs", ejsMate);
 //setting our view engine to ejs
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 //joining our views path to our working directory, establishing an absolute path to views
-app.set('views', path.join(__dirname, 'views'))
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
-app.get('/', (req, res) =>
-{
-    res.render("home")
-})
+app.get(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
 
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
 
-
-app.get('/campgrounds', async (req, res) =>
-{
-    try
-    {
-        const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", {campgrounds})
-    } catch (e)
-    {
-        console.log(e)
-    }
-})
-
-app.get('/campgrounds/new', (req, res) =>
-{
-    res.render('campgrounds/new')
-})
-
-app.post('/campgrounds', async (req, res) =>
-{
-   
-    const campground = new Campground(req.body.campground)
+app.post(
+  "/campgrounds",
+  catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+    const campground = new Campground(req.body.campground);
     await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-     
-})
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
-app.get('/campgrounds/:id', async (req, res) =>
-{
-    try
-    {
-        const id = req.params.id;
+app.get(
+  "/campgrounds/:id",
+  catchAsync(async (req, res, next) => {
+    const id = req.params.id;
     const campground = await Campground.findById(id);
-    res.render('campgrounds/show', {campground})
-    } catch (e)
-    {
-        console.log(e)
-   }
-})
+    res.render("campgrounds/show", { campground });
+  })
+);
 
-app.get('/campgrounds/:id/edit', async (req, res) =>
-{
-    try
-    {
-        const id = req.params.id;
+app.get(
+  "/campgrounds/:id/edit",
+  catchAsync(async (req, res, next) => {
+    const id = req.params.id;
     const campground = await Campground.findById(id);
-    res.render('campgrounds/edit', {campground})
-    } catch (e)
-    {
-        console.log(e)
-    }
-})
+    res.render("campgrounds/edit", { campground });
+  })
+);
 
 //put route through methodOverride on form, with POST method
-app.put('/campgrounds/:id', async (req, res) =>
-{
+app.put(
+  "/campgrounds/:id",
+  catchAsync(async (req, res, next) => {
     const { id } = req.params;
     //find and update by id and spread request body for changes
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground })
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
 
+app.delete(
+  "/campgrounds/:id",
+  catchAsync(async (req, res, next) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id, { useFindAndModify: false });
+    res.redirect("/campgrounds");
+  })
+);
 
-app.delete('/campgrounds/:id', async (req, res)=> {
-    try
-    {
-        const { id } = req.params;
-    await Campground.findByIdAndDelete(id, {useFindAndModify: false})
-    res.redirect('/campgrounds')
-    } catch (e)
-    {
-        console.log(e)
-    }
-})
+//routes all htttp requests which were not found
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+});
 
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = "Oh No, Something went wrong!"
+  res.status(statusCode).render('error', {err});
+});
 
-
-app.listen(3000, () =>
-{
-    console.log("Serving on Port 3000!")
-})
-
+app.listen(3000, () => {
+  console.log("Serving on Port 3000!");
+});
