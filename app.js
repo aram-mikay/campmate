0b24ceebcfdd792149b119628c1c58ea251ae873
Aram Mikayelian <arammikayelian@gmail.com>
2021-04-19 11:08:28 -0700

    Added server side form validation with JOI

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const {campgroundSchema} = require('./schemas')
 const ejsMate = require("ejs-mate");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
@@ -31,6 +32,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const validateCampground = (req, res, next) =>
+{
+  const { error } = campgroundSchema.validate(req.body)
+  if (error)
+  {
+    const msg = error.details.map(el => el.message).join(',')
+    throw new ExpressError(msg,400)
+  } else
+  {
+    next();
+  }
+}
+
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -48,9 +63,10 @@ app.get("/campgrounds/new", (req, res) => {
 });
 
 app.post(
-  "/campgrounds",
-  catchAsync(async (req, res, next) => {
-    if (!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+  "/campgrounds", validateCampground,
+  catchAsync(async (req, res, next) =>
+  {
+    
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -77,7 +93,7 @@ app.get(
 
 //put route through methodOverride on form, with POST method
 app.put(
-  "/campgrounds/:id",
+  "/campgrounds/:id", validateCampground,
   catchAsync(async (req, res, next) => {
     const { id } = req.params;
     //find and update by id and spread request body for changes
